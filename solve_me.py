b4e094d3fd67daf9c9b7a26b3faff71a76d163e4
Ellie Rinehart <erinehart@salesforce.com>
2025-05-01 11:40:40 -0400

    Create solve_me.py

---
+++ solve_me.py
@@ -0,0 +1,9 @@
+from key_gen import generate_key
+
+# Passphrase riddle
+print('Riddle: How do you say SGVsbG8sIFdvcmxkIQ== in English?')
+
+passphrase = input("Enter the passphrase: ")
+
+# Check if the passphrase is correct and generate the key (offset)
+key = generate_key(passphrase)
